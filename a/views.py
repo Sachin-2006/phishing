b9c06bce4a2b0d1bde7950c4bdbb5019c131ad06
Sachin-2006 <72447101+Sachin-2006@users.noreply.github.com>
2023-10-14 15:45:07 +0530

    Update views.py

--- a/views.py
+++ a/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(req):
 	if req.method == "GET":
-		print(req.GET['passwrd'])
-		
+		print("Username : ",req.GET['username'])
+		print("Password : ",req.GET['passwrd'])
 	return render(req,"index.html")
